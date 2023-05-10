06b1f2b9d57ae5c731ba40df03e949e20a244ddb
Kirshi Yin <kristina.nikolova.ext@dwpbank.de>
2023-05-10 16:45:11 +0200

    refactored method return value in rest controller

--- src/main/java/com/jpainheritance/demo/controller/DemoController.java
+++ src/main/java/com/jpainheritance/demo/controller/DemoController.java
@@ -1,10 +1,13 @@
 package com.jpainheritance.demo.controller;
 
+import com.jpainheritance.demo.entity.ConcreteEntityA;
 import com.jpainheritance.demo.entity.ConcreteEntityB;
 import com.jpainheritance.demo.service.DemoService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class DemoController {
@@ -16,13 +19,13 @@ public class DemoController {
     }
 
     @GetMapping(path = "/a")
-    public String getAAttributes() {
-        return demoService.getA().toString();
+    public List<ConcreteEntityA> getAAttributes() {
+        return demoService.getA();
     }
 
     @GetMapping(path = "/b")
-    public String getBAttributes() {
-        return demoService.getB().toString();
+    public List<ConcreteEntityB> getBAttributes() {
+        return demoService.getB();
     }
 
     @GetMapping(path = "/b/number")
